123a27788e032b39abc02983ea2832d06f7c4cd9
Jericho <jerichobantiquete@gmail.com>
2023-10-17 00:34:17 +0800

    Java Objects to JSON Objects

--- src/main/java/com/monciego/Main.java
+++ src/main/java/com/monciego/Main.java
@@ -1,5 +1,7 @@
 package com.monciego;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,19 @@ public class Main {
 
     @GetMapping("/")
     public GreetingResponse greet() {
-        return new GreetingResponse("Hello");
+        GreetingResponse response = new GreetingResponse(
+            "Hello",
+            List.of("Golang", "Rust", "C++", "TypeScript"),
+            new Person("Jericho", "jerichobantiquete@gmail.com", 23)
+        );
+        return response;
     }
 
-    record GreetingResponse(String greet){}
+    record Person (String name, String email, int age) {}
+
+    record GreetingResponse(
+        String greet,
+        List<String> favProgrammingLanguages,
+        Person person
+    ){}
 }
